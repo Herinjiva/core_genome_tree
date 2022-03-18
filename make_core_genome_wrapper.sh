6156674ac04bffae6a74686e8af8e65de7ab42f3
ANDRIAMANGA <vahiniaina-herinjiva.andriamanga@universite-paris-saclay.fr>
2022-03-18 18:41:45 +0100

    Update make_core_genome_wrapper.sh

--- make_core_genome_wrapper.sh
+++ make_core_genome_wrapper.sh
@@ -14,4 +14,4 @@ echo "$run_name"
 echo "$core_output"
 
 python3 make_core_genome.py $bl_output $run_name $core_output
-python3 core_extract.py -core_list "${core_output}/core_${run_name}" -homolog "${core_output}/homolog_${run_name}" -taxa_name prot
+python3 core_extract.py -core_list "${core_output}core_${run_name}" -homolog "${core_output}homolog_${run_name}" -taxa_name prot
